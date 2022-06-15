62198a75d3e489ff8151ca07db4b228a41e44953
cozy-miura <107518322+cozy-miura@users.noreply.github.com>
2022-06-15 15:36:06 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Hoge Fuga CozyMiura.
+  Hoge Fuga CozyMiura 2022/6/15.
   <!-- END -->
 
   </div>
